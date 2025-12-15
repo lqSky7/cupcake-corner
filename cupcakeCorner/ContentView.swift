4eaec2e931d85907a0bb6d39f533e4c7f9320e0d
ca5 <catinice@outlook.com>
2025-12-15 09:46:25 +0530

    Sending and receiving Codable data with URLSession
    Signed-off-by: ca5 <catinice@outlook.com>
    
    Change-Id: I53a4f1c50cb654fd303218849caaa47f07751f2a

--- cupcakeCorner/ContentView.swift
+++ cupcakeCorner/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct result : Codable {
+    let trackId : Int
+    let trackName : String
+    let collectionName : String
+}
+struct response : Codable {
+    let results : [result]
+}
+
 struct ContentView: View {
+    @State private var resArray : [result] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List{
+            VStack(alignment: .leading){
+                ForEach(resArray, id: \.trackId){
+                    k in
+                    Text(k.trackName)
+                        .font(.title)
+                    Text(k.collectionName)
+                        .font(.headline)
+                    Divider()
+                }
+                
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=linkin+park&entity=song") else {
+            print("error")
+            return
+        }
+        
+        do {
+            let (data, _ ) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(response.self, from: data) {
+                resArray = decodedResponse.results
+            }
+        } catch {
+            print(error)
+        }
+        
     }
 }
 
