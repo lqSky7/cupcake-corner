0752bc14c5598a09836e43b088848db6c00830e1
ca5 <catinice@outlook.com>
2025-12-15 09:09:55 +0530

    Initial Commit
    Change-Id: I9823e78d64735d6dc95fedf1ce54128bf1279a73

---
+++ cupcakeCorner/cupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcakeCornerApp.swift
+//  cupcakeCorner
+//
+//  Created by ca5 on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct cupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
