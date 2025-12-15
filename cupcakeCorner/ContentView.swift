75e28cb87a9480023056c55fef3dbe94547eadcc
ca5 <catinice@outlook.com>
2025-12-15 10:55:12 +0530

    easy haptics
    Signed-off-by: ca5 <catinice@outlook.com>
    
    Change-Id: I7503eec745f56f16a1924a06c366503ec0067b47

--- cupcakeCorner/ContentView.swift
+++ cupcakeCorner/ContentView.swift
@@ -7,80 +7,30 @@
 
 import SwiftUI
 
-struct result : Codable {
-    let trackId : Int
-    let trackName : String
-    let collectionName : String
+@Observable
+class user : Codable {
+    enum CodingKeys : String, CodingKey {
+        case _name = "name"
+    }
+    var name = "test"
 }
-struct response : Codable {
-    let results : [result]
-}
+
 
 struct ContentView: View {
-    @State private var userN = ""
-    @State private var pwd = ""
-    private var disableButton : Bool {
-        userN.isEmpty || pwd.count < 5
-    }
-    @State private var resArray : [result] = []
+    @State private var tes = 0
     var body: some View {
         
-        VStack{
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
-            } .frame(maxWidth: .infinity, maxHeight: 200)
-                .background(.ultraThickMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .padding()
-                .zIndex(1)
+        Button ("encode test"){
+            tes += 1
         }
-
-        List{
-            VStack(alignment: .leading){
-                Section {
-                    TextField("Name", text: $userN)
-                    TextField("pass", text: $pwd)
-                    Button("login") {
-                        print("hi")
-                    }
-                    .disabled(disableButton)
-                }
-                
-                ForEach(resArray, id: \.trackId){
-                    k in
-                    Text(k.trackName)
-                        .font(.title2)
-
-                    Text(k.collectionName)
-                        .font(.caption)
-                    Divider()
-                }
-                
-            }
-        }
-        .task {
-            await loadData()
-        }
+        .sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: tes)
+            
     }
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=linkin+park&entity=song") else {
-            print("error")
-            return
-        }
         
-        do {
-            let (data, _ ) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(response.self, from: data) {
-                resArray = decodedResponse.results
-            }
-        } catch {
-            print(error)
-        }
-        
+    func encd(){
+        let data  = try! JSONEncoder().encode(user())
+        let decoded = String(decoding: data, as: UTF8.self)
+        print(decoded)
     }
 }
 
